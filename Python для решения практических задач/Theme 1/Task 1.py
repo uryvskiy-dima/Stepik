e023340a742284889f31cb2f2143a7bd3775ac79
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-22 17:39:19 +0300

    Update Task 1

--- Python для решения практических задач/Theme 1/Task 1.py
+++ Python для решения практических задач/Theme 1/Task 1.py
@@ -9,7 +9,6 @@ https://stepik.org/media/attachments/lesson/209717/1.html
 
 from urllib.request import urlopen
 
-
 html = urlopen("https://stepik.org/media/attachments/lesson/209717/1.html").read().decode('utf-8')
 print("C++ -", str(html).count("C++"))
 print("Python -", str(html).count("Python"))
