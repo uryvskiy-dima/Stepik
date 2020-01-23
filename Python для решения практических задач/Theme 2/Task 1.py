08aaa984d74cbe7c5a835405800c0d9a2c86727d
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-23 14:25:06 +0300

    Add Task 6

---
+++ Python для решения практических задач/Theme 2/Task 1.py
@@ -0,0 +1,17 @@
+"""
+Для решения этой задачи необходимо установить библиотеку xlrd, скачать файл
+https://stepik.org/media/attachments/lesson/245266/tab.xlsx и создать в папке с этим файлом скрипт
+со следующем содержанием:
+"""
+
+
+import xlrd
+
+wb = xlrd.open_workbook('tab_for_task1.xlsx')
+sheet_names = wb.sheet_names()
+sh = wb.sheet_by_name(sheet_names[0])
+nmin = sh.row_values(6)[2]
+for rownum in range(7, 27):
+    temp = sh.row_values(rownum)
+    nmin = min(nmin, temp[2])
+print(nmin)
