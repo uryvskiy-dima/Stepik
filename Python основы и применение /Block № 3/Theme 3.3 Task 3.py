aa6ef4fc4723a5166d86961f5b7ccae7fbe5fb6d
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-27 23:25:41 +0300

    Updated Theme 3.3

--- Python основы и применение /Block № 3/Theme 3.3 Task 3.py
+++ Python основы и применение /Block № 3/Theme 3.3 Task 3.py
@@ -19,8 +19,7 @@ zzxzz
 import sys
 import re
 
-pattern = r"z.{3}z"
 for line in sys.stdin:
     line = line.rstrip()
-    if re.search(pattern, line) is not None:
+    if re.search(r"z.{3}z", line) is not None:
         print(line)
