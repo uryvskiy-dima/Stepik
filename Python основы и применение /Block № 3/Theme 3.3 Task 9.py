a888a95a197ba80115fa231a954722c8ac46ba5f
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-28 14:22:56 +0300

    Updated Theme 3.3

--- Python основы и применение /Block № 3/Theme 3.3 Task 9.py
+++ Python основы и применение /Block № 3/Theme 3.3 Task 9.py
@@ -16,7 +16,6 @@ buz
 import re
 import sys
 
-pattern = r"(\w)\1+"
 for line in sys.stdin:
     line = line.rstrip()
-    print(re.sub(pattern, r"\1", line))
+    print(re.sub(r"(\w)\1+", r"\1", line))
