c39a2cadaac9e78b29861d3a959372305bc7038f
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-12 00:36:31 +0300

    Add Task 40

--- Python Адаптивный тренажер/Task 40.py
+++ Python Адаптивный тренажер/Task 40.py
@@ -54,3 +54,22 @@ XXXX.X
 XXXXX.
 """
 
+
+def game_of_life(row, column):
+    count = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if i != 0 or j != 0:
+                if start_field[(row + i + n) % n][(column + j + m) % m] == 'X':
+                    count += 1
+
+    return 'X' if start_field[row][column] == 'X' and count == 2 or count == 3 else '.'
+
+
+n, m = map(int, input().split())
+#  получил начальное поле
+start_field = [list(input()) for i in range(n)]
+#  алгоритм поиска соседей
+lst = [[game_of_life(i, j) for j in range(m)] for i in range(n)]
+#  вывод результата
+[print(*row, sep='') for row in lst]
