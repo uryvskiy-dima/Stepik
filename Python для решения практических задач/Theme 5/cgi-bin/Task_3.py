50d2eff1d7efd11143c5e5b1355c9319d76292f9
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-18 02:34:00 +0300

    Add Task 3

---
+++ Python для решения практических задач/Theme 5/cgi-bin/Task_3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html")
+print()
+print("<html>")
+print("<body>")
+print("<table>")
+for i in range(1, 11):
+    print("<tr>")
+    for j in range(1, 11):
+        print(f"<td><a href=>{i * j}</a></td>")
+    print("</tr>")
+print("</table>")
+print("</body>")
+print("</html>")
