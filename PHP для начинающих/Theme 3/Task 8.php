39330effbc120c4ae3b170957a7eaaa788aaf3b2
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-03-12 00:33:06 +0300

    Add Task 8

--- PHP для начинающих/Theme 3/Task 8.php
+++ PHP для начинающих/Theme 3/Task 8.php
@@ -1,1 +1,7 @@
 <?php
+fscanf(STDIN, "%d", $Ch);
+$a = $Ch % 10;
+$b = $Ch / 10 % 10;
+$c = $Ch / 100 % 10;
+echo max($a, $b, $c), ($a + $b + $c) - max($a, $b, $c) - min($a, $b, $c), min($a, $b, $c);
+?>
